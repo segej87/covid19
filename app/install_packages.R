f217493f562dfe0763a923c0f520a57464e1f23a
Jon <jonsege@gmail.com>
2020-03-28 01:48:38 -0400

    allow installation from binary

--- app/install_packages.R
+++ app/install_packages.R
@@ -4,6 +4,14 @@ library(yaml)
 
 cfg <- yaml::read_yaml('cfg.yml')
 
-packages <- unlist(cfg['packages'])
+install_from_binary <- unlist(cfg['install_from_binary'])
 
-install.packages(packages, repos = 'http://lib.stat.cmu.edu/R/CRAN/', verbose = FALSE)
+if (install_from_binary) {
+  packages <- list.files('packages')
+  repos <- NULL
+} else {
+  packages <- unlist(cfg['packages'])
+  repos <- 'http://lib.stat.cmu.edu/R/CRAN/'
+}
+
+install.packages(pkgs = packages, repos = repos, verbose = FALSE)
