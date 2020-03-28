9b9a39de9ba432e6437a22ade9a0b6c579eaa6a3
Jon <jonsege@gmail.com>
2020-03-28 01:57:21 -0400

    fix binary install

--- app/install_packages.R
+++ app/install_packages.R
@@ -7,7 +7,7 @@ cfg <- yaml::read_yaml('cfg.yml')
 install_from_binary <- unlist(cfg['install_from_binary'])
 
 if (install_from_binary) {
-  packages <- list.files('packages')
+  packages <- file.path('packages', list.files('packages'))
   repos <- NULL
 } else {
   packages <- unlist(cfg['packages'])
